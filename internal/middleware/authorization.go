cc6b4d6077d97603416e3a722b71c5ac6e2cd3b0
Max McFerren <maxmcferren@Maxs-MBP.lan>
2024-04-24 16:24:56 -0400

    Basic api setup

---
+++ internal/middleware/authorization.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/mmcferren/goapi/api"
+	"github.com/mmcferren/goapi/internal/tools"
+	log "github.com/sirupsen/logrus"
+)
+
+var UnauthorizedError = errors.New("invalid username or token")
+
+func Authorization(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error
+
+		if username == "" || token == "" {
+			log.Error(UnauthorizedError)
+			api.RequestErrorHandler(w, UnauthorizedError)
+			return
+		}
+
+		var database *tools.DatabaseInterface
+		database, err = tools.NewDatabase()
+		if err != nil {
+			api.InternalErrorHandler(w)
+			return
+		}
+
+		var loginDetails *tools.LoginDetails
+		loginDetails = (*database).GetUserLoginDetails(username)
+
+		if loginDetails == nil || token != (*loginDetails).AuthToken {
+			log.Error(UnauthorizedError)
+			api.RequestErrorHandler(w, UnauthorizedError)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
